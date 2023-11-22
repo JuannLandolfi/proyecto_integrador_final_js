068985b86ca2b34da32f1098df20cfa5b66caf43
JuannLandolfi <juanagustinlandolfi1998@gmail.com>
2023-11-22 16:08:51 -0300

    cambios para la reentrega

---
+++ README.md
@@ -0,0 +1,48 @@
+README.md - Documentación de E-commerce
+
+
+
+Descripción del Proyecto
+
+Este proyecto es un e-commerce que permite a los usuarios explorar y comprar productos en línea. La aplicación utiliza tecnologías web como JavaScript para interactuar con una API y proporcionar una experiencia de compra.
+
+Características Clave
+1.Catálogo de Productos: La tienda en línea ofrece una amplia gama de productos, desde micrófonos y auriculares hasta parlantes, organizados en diferentes categorías.
+
+2.Filtrado por Categoría: Los usuarios pueden explorar productos por categoría, lo que facilita la búsqueda de artículos específicos.
+
+3.Agregar al Carrito: Los usuarios pueden agregar productos a su carrito de compras con un solo clic.
+
+4.Visualización de Detalles: La aplicación permite a los usuarios ver detalles completos de un producto seleccionado.
+
+5.Carrito de Compras: Los productos seleccionados se agregan al carrito, donde los usuarios pueden ver la cantidad y el precio total.
+
+6.Eliminar del Carrito: Los usuarios pueden eliminar productos no deseados del carrito.
+
+7.Total de Compra: La aplicación muestra el precio total de todos los productos en el carrito.
+
+8.Comprar Productos: Los usuarios pueden proceder a comprar los productos en su carrito.
+
+
+
+Tecnologías y Conceptos Clave
+
+- JavaScript: El proyecto se desarrolló principalmente utilizando JavaScript para la lógica de la tienda en línea.
+
+- Fetch API: La Fetch API se utiliza para obtener datos de productos desde un archivo JSON externo.
+
+- AJAX (Asynchronous JavaScript and XML): Se utiliza AJAX para cargar datos de productos sin recargar la página.
+
+- Objetos en JavaScript: Se crean y manipulan objetos de JavaScript para representar productos y categorías.
+
+- Promesas: Se utilizan promesas para manejar las solicitudes asincrónicas y las respuestas de la API.
+
+- Eventos: Los eventos se emplean para activar acciones cuando los usuarios hacen clic en los botones de agregar al carrito o eliminar del carrito.
+
+
+Uso del Proyecto
+1.Clona este repositorio en tu máquina local.
+
+2.Abre el archivo index.html en un navegador web.
+
+3.Explora el catálogo de productos, agrega productos al carrito y realiza compras simuladas.
--- js/index.js
+++ js/index.js
@@ -1,50 +1,13 @@
-//Productos en Arrays
+// Declaración de un arreglo para almacenar los productos.
+let productosArray = [];
 
-const productosArray = [
-    {
-        id: "monitor-gdr",
-        titulo: "Monitor GDR",
-        imagen: "./multimedia/img_monitor1.jpg",
-        categoria: {
-            nombre: "Monitores",
-            id: "monitores"
-        },
-        precio: 56000,
-    },
-
-    {
-        id: "teclado-gammer",
-        titulo: "Teclado Gammer",
-        imagen: "./multimedia/img_teclado1.jpg",
-        categoria: { 
-            nombre: "Teclados",
-            id: "teclados"
-        },
-        precio: 48500,
-    },
-
-    {
-        id: "mouse-u2",
-        titulo: "Mouse U2",
-        imagen: "./multimedia/img_mouse1.jpg",
-        categoria: {
-            nombre: "Mouses",
-            id: "mouses"
-    },
-    precio: 22700,
-},
-
-{
-    id: "monitor-dd",
-    titulo: "Monitor DD",
-    imagen: "./multimedia/img_monitor2.jpg",
-    categoria: { 
-        nombre: "Monitores",
-        id: "monitores",
-},
-    precio: 61000,
-},
-];
+// Realizar una solicitud fetch para obtener datos de un archivo JSON.
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productosArray = data;
+        cargarProductos(productosArray);
+    })
 
 
 
@@ -75,11 +38,22 @@ function cargarProductos(productosArray) {
             <h3 class="producto_titulos">${producto.titulo}</h3>
             <p class="producto_precio">${producto.precio}</p>
             <button class="producto_boton" id="${producto.id}">Agregar al carrito</button>
+            <button class="producto_detail" id="${producto.id}">Detalles</button>
         </div>
         `;
 
+
+        
+    // Agregar un controlador de eventos para mostrar detalles del producto.
+    const botonDetalles = div.querySelector(".producto_detail");
+    botonDetalles.addEventListener("click", () => {
+        mostrarDetallesProducto(producto);
+    });
+
+     // Agregar el producto al contenedor.
         contenedorProductos.append(div);
     });
+
     actualizarBotonesAgregar();
 
 }
@@ -149,6 +123,46 @@ function agregarAlCarrito(e) {
 function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce ((acc, producto) => acc + producto.cantidad, 0);
     numerito.innerText = nuevoNumerito;      //hacemos que la cantidad de productos se vea reflejada en el numero asignado al carrito
+}
+
+
+
+// Función para mostrar detalles de un producto en una ventana emergente.
+function mostrarDetallesProducto(producto) {
+    const ventanaDetalles = window.open('', 'ventanaDetalles');
+    
+    ventanaDetalles.document.write(`
+        <html>
+        <head>
+            <title>Detalles del Producto</title>
+            <link rel="stylesheet" href="./css/detalles.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+        </head>
+        <body>
+            <div class="contenedor">
+                <aside>
+                    <header>
+                        <h1 class="logo">Landolfi</h1>
+                    </header>
+                    <nav>
+                    <a class="boton_general boton_carrito" href="./index.html"><i class="bi bi-box-arrow-left"></i> Volver al inicio</a>
+                    </nav>
+                    <div class="tarjeta_detalle">
+                    <h1 class="subtitulo">Detalles del producto</h1>
+                    <h3 class="producto_titulo">${producto.titulo}</h3>
+                    <img class="producto_imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <p>Precio: ${producto.precio}</p>
+                    <p>Descripción: ${producto.detalles.descripcion}</p>
+                    <p>Características:</p>
+                        <ul>
+                            ${producto.detalles.caracteristicas.map(caracteristica => `<li>${caracteristica}</li>`).join('')}
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </body>
+        </html>
+    `);
 }
 
 
